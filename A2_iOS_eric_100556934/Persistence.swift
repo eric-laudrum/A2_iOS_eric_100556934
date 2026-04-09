97839b9caae8b01e8c6b34e54858d77ddadb2306
eric-laudrum <ericlaudrum@gmail.com>
2026-04-09 13:49:30 -0400

    update: set up initial persistence controller with initialization

--- A2_iOS_eric_100556934/Persistence.swift
+++ A2_iOS_eric_100556934/Persistence.swift
@@ -1,7 +1,25 @@
-//
 //  Persistence.swift
 //  A2_iOS_eric_100556934
 //
 //  Created by Eric Laudrum on 4/9/26.
 //
+
+import CoreData
+
+struct PersistenceController{
+    
+    static let shared = PersistenceController()
+    
+    let container: NSPersistentContainer
+    
+    init(){
+        container = NSPersistentContainer( name: "ProductModel")
+        container.loadPersistentStores{
+            (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Error: \(error)")
+            }
+        }
+    }
+}
 
