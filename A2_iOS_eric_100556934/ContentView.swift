20d9fd725a4dd362ac6aceb7c2d52c3a71b9dadb
eric-laudrum <ericlaudrum@gmail.com>
2026-04-09 16:14:58 -0400

    add: content view layout for the list with issues running in simulator

--- A2_iOS_eric_100556934/ContentView.swift
+++ A2_iOS_eric_100556934/ContentView.swift
@@ -10,6 +10,8 @@ import CoreData
 
 struct ContentView: View {
     
+    // Database context
+    @Environment(\.managedObjectContext) private var viewContext
     
     // Fetch all products
     @FetchRequest(
@@ -17,13 +19,14 @@ struct ContentView: View {
     
     private var products: FetchedResults<Product>
     
+    
     var body: some View {
         NavigationStack{
             
             List{
                 ForEach(products) { product in
                     NavigationLink{
-                        ProductDetailView(product: product) // TBD
+                        ProductDetailView(product: product)
                     } label: {
                         VStack(){
                             Text(product.productName ?? "product")
@@ -34,11 +37,37 @@ struct ContentView: View {
                     }
                 }
             }
+            .navigationTitle("Products")
         }
     
     }
 }
 
+struct ProductDetailView: View{
+    let product: Product
+    
+    var body: some View{
+        List {
+                    Section(header: Text("Product Info")) {
+                        Text(product.productName ?? "No Name")
+                            .font(.title)
+                        Text("Provider: \(product.productProvider ?? "N/A")")
+
+                        Text("Price: $\(product.productPrice, specifier: "$.2f")")
+                    }
+                    
+                    Section(header: Text("Description")) {
+                        Text(product.productDescription ?? "No description provided.")
+                    }
+                }
+                .navigationTitle("Details")
+        
+            }
+        }
+
+
 #Preview {
-    ContentView()
+    ContentView().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
 }
+
+
